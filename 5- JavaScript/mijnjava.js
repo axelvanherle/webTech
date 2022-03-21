6cbad3c2b4a9575cfc1da7f2f8f12d3c9306ca3e
Axel Vanherle <axelvanherle@outlook.com>
2022-03-21 16:25:59 +0100

    update

---
+++ 5- JavaScript/mijnjava.js
@@ -0,0 +1,7 @@
+function eersteKnop(){
+    document.getElementById("aEersteknop").innerHTML = "Je hebt op de knop gedrukt!"
+};
+
+function introKnop(){
+    document.getElementById("introKnopa").innerHTML = "Je hebt op de knop gedrukt!"
+};
